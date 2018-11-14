1f9d014010a7cda7581717262024c3cf0c4ddee6
earlfernando <42997598+earlfernando@users.noreply.github.com>
2018-11-14 14:09:17 +0100

    Delete pflat.m

--- assignment1/pflat.m
+++
@@ -1,6 +0,0 @@
-function out = pflat(coordinates)
-    last_row = coordinates(end,:);
-    dimension_of_input = size(coordinates);
-    division_term = repmat(last_row,dimension_of_input(1),1);
-    out = coordinates./ division_term;
-end
