e7c54829e3ff87c45ccb2538501aab53d3fa403f
andrea <andrea_ramazzina@hotmail.it>
2018-11-19 11:22:49 +0100

    continuation Ex 5

--- assignment1/assignment1_andrea/Exercise_5.m
+++ assignment1/assignment1_andrea/Exercise_5.m
@@ -30,14 +30,36 @@ axis1 = [0;0;1];
 figure(3)
 plot3( points3D(1 ,[1: end 1]) , points3D(2 ,[1: end 1]), points3D(3 ,[1: end 1]) , '*-' )
 axis('equal')
+axis ij
 hold on 
 quiver3(C(1), C(2), C(3), axis1(1), axis1(2), axis1(3), 5)
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5%%%%  
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  
 R = [sqrt(3)/2, 0, 0.5; 0, 1, 0; -0.5, 0, sqrt(3) /2];
-t = [0;0;0];
+
+t = [0;0;0] - R*[2;0;0];
 RT = [R,t];
 
+H = R- t*pi' ;
+
+new_new_corners = H*new_corners;
+new_new_corners = pflat(new_new_corners);
+figure(4)
+plot ( new_new_corners(1 ,[1: end 1]) , new_new_corners(2 ,[1: end 1]) , 'r*-' );
+axis ij
+hold on
+
+punti = RT*[points3D;ones(1,4)];
+punti = pflat(punti);
+plot ( punti(1 ,[1: end 1]) , punti(2 ,[1: end 1]) , 'g*-' );
 
 
+tform = maketform ( ’ projective ’, Htot ’);
+% Creates a projective transformation that can be used in imtransform
+% NOTE : Matlab uses the transposed version of the homografi .
+[ new_im , xdata , ydata ] = imtransform (im , tform , ’ size ’, size ( im ));
+% Creastes a transformed image ( using tform )
+% of the same size as the original one .
+imagesc ( xdata , ydata , new_im );
+% plots the new image with xdata and ydata on the axes
