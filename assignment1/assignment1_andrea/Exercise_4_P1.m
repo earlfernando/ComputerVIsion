e7c05eeab55ffb87f535bbcd406b70558e1ab66d
andrea <andrea_ramazzina@hotmail.it>
2018-11-20 17:25:26 +0100

    fix final things

--- assignment1/assignment1_andrea/Exercise_4_P1.m
+++ assignment1/assignment1_andrea/Exercise_4_P1.m
@@ -33,6 +33,7 @@ y = U_norm(2,:);
 z = U_norm(3,:);
 scatter3(x,y,z,'.')
 hold on
+axis equal
 
 %Get the center of the camera:
 a = P1(:,4);
@@ -46,6 +47,18 @@ C_B = C_B(1:3);
 
 quiver3(C_A(1), C_A(2), C_A(3), R(3,1), R(3,2), R(3,3), 10)
 
-axis = R(3,1:3); %Already normalized to length 1
+axis1 = R(3,1:3); %Already normalized to length 1
 %Alternatively:
 axis_B = P1(3,1:3);
+
+%Projecting:
+projection = P1*U;
+projection = pflat(projection);
+projection = projection(1:2,1:end);
+
+A = imread('compEx4im1.JPG');
+B = imread('compEx4im2.JPG');
+imshow(A, 'InitialMagnification',150)
+hold on
+plot(projection(1,1:end),projection(2,1:end),'r.' )
+axis equal
