02abbd8064f30a08b105475f44f704e2eeba4c6a
andrea <andrea_ramazzina@hotmail.it>
2018-11-28 14:15:23 +0100

    extend ex3 and ex4

--- assignment2/assignment2_andrea/Computer_Exercise_3.m
+++ assignment2/assignment2_andrea/Computer_Exercise_3.m
@@ -5,9 +5,10 @@ load('compEx3data.mat')
 %Get camera matrices:
 plot_result = false;
 plot_normalized_points = false;
-normalize = true;
-P_1 = get_P(1, normalize, plot_normalized_points, plot_result)
-P_2 = get_P(2, normalize, plot_normalized_points, plot_result)
+normalize = false;
+remotion = true;
+P_1 = get_P(1, normalize, plot_normalized_points, plot_result,remotion);
+P_2 = get_P(2, normalize, plot_normalized_points, plot_result,remotion);
 
 %Get camera centers:
 C_1 = null(P_1);
@@ -21,21 +22,41 @@ A_2 = P_2(3,1:3);
 
 %
 figure
-hold on
+hold on;
 quiver3(C_1(1), C_1(2), C_1(3), A_1(1), A_1(2), A_1(3), 10000)
-hold on
+hold on;
 quiver3(C_2(1), C_2(2), C_2(3), A_2(1), A_2(2), A_2(3), 10000)
 
 plot3([ Xmodel(1 , startind ); Xmodel(1 , endind )] ,...
 [ Xmodel(2 , startind ); Xmodel(2 , endind )] ,...
 [ Xmodel(3 , startind ); Xmodel(3 , endind )] , 'b - ' );
+hold off;
 
+[r,q]=rq(P_1(1:3,1:3));
+K_1 = r; 
+R_1 = q;
+[r,q]=rq(P_2(1:3,1:3));
+K_2 = r; 
+R_2 = q;
 
-function P = get_P(i, normalize, plot_normalized_points, plot_result)
+function e = compute_error(xm, xp)
+ 
+n = size(xm,2);
+e = norm(xm-xp)^2;
+e = sqrt(e/n);
 
+end
+function P = get_P(i, normalize, plot_normalized_points, plot_result,remotion)
+    numero = i;
     load('compEx3data.mat')
     pointss = cell2mat(x(i));
-
+    if remotion == true
+        %remove points expect the listed ones:
+        pointss = pointss(1:end,[1, 4, 13, 16, 25, 28, 31]);
+        Xmodel = Xmodel(1:end,[1, 4, 13, 16, 25, 28, 31]);
+    end
+    number_of_points = size(pointss,2);
+    
     if (i ==1)
         img = imread('cube1.JPG');
     else
@@ -56,15 +77,14 @@ function P = get_P(i, normalize, plot_normalized_points, plot_result)
     points_normalized = N*cell2mat(x(i));   
     
     %--Construct M:
-    Xmodel(4,1:37) = ones(1,37);
+    Xmodel(4,1:number_of_points) = ones(1,number_of_points);
 
-    for i = 1:37
-
+    for i = 1:number_of_points
         M(3*(i-1)+1, 1:12) = [ Xmodel(1:4,i)',zeros(1,8)  ] ; 
         M(3*(i-1)+2, 1:12) = [zeros(1,4), Xmodel(1:4,i)',zeros(1,4)    ] ; 
         M(3*(i-1)+3, 1:12) = [zeros(1,8), Xmodel(1:4,i)'] ; 
     end
-    for i=1:37
+    for i=1:number_of_points
         x = points_normalized(1,i);
         y = points_normalized(2,i);
         M(3*(i-1)+1,12+i) = -x;
@@ -90,8 +110,8 @@ function P = get_P(i, normalize, plot_normalized_points, plot_result)
         figure
         imshow(img)
         hold on
-        plot(pointss(1,1:37),pointss(2,1:37),'og')
-        plot(new_points_new(1,1:37),new_points_new(2,1:37),'or')
+        plot(pointss(1,1:number_of_points),pointss(2,1:number_of_points),'og')
+        plot(new_points_new(1,1:number_of_points),new_points_new(2,1:number_of_points),'or')
     end
     
     if plot_normalized_points == true     
@@ -101,4 +121,8 @@ function P = get_P(i, normalize, plot_normalized_points, plot_result)
         %Check the mean distance from the center:
         mean(sqrt(points_normalized(1,:).^2 + points_normalized(2,:).^2) )
     end    
+    
+    e = compute_error(pointss, new_points_new);
+    fprintf("\nError for %d img is %f:",numero,e);
+    
 end
--- assignment2/assignment2_andrea/Computer_Exercise_4.m
+++ assignment2/assignment2_andrea/Computer_Exercise_4.m
@@ -1,5 +1,5 @@
-clear all 
-close all
+%clear all 
+%close all
 
 load('compEx3data.mat')
 img_1 = imread('cube1.JPG');
