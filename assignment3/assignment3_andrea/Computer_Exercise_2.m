b81fff26b32e088221a7b838f6709a3927847d84
andrea <andrea_ramazzina@hotmail.it>
2018-12-05 15:47:34 +0100

    Fixed Comp Ex 2

--- assignment3/assignment3_andrea/Computer_Exercise_2.m
+++ assignment3/assignment3_andrea/Computer_Exercise_2.m
@@ -8,14 +8,16 @@ P_1 = [eye(3),[0;0;0]];
 e2 = null(F');
 P_2 = [get_matr_cross(e2)*F, e2];
 
-
+%P_2 = [ -0.0016    0.0057    0.2163    0.9763
+%    0.0070   -0.0257   -0.9763    0.2163
+%    0.0000    0.0000   -0.0273    0.0001 ]
 
 
 for i =1:size(x1_norm,2)
 
     point_1 = [x1_norm(1:2,i);1];
     point_2 = [x2_norm(1:2,i);1];
-    M = [P_1;P_2];
+    M = [N1*P_1;N2*P_2];
     M(1:3,5)= -point_1;
     M(4:end,6)= -point_2;
 
@@ -27,10 +29,22 @@ for i =1:size(x1_norm,2)
     
 end
 
+x_projection1 = pflat(P_1*X);
+x_projection2 = pflat(P_2*X);
+
+figure(1)
+img = imread('kronan1.JPG');
+imshow(img)
+hold on
+plot(x1(1,1:end), x1(2,1:end),'ob')
+hold on
+plot(x_projection1(1,1:end), x_projection1(2,1:end),'xr')
 
 
-
-
+figure(2)
+plot3(X(1,1:end),X(2,1:end),X(3,1:end),'.')
+axis ij
+axis equal 
 function A = get_matr_cross(y)
     A = zeros(3);
     A(1,2) = -y(3); 
