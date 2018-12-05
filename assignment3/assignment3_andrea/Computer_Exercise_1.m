4f74468ea282e156aa1f0fd78718f04daeacf405
andrea <andrea_ramazzina@hotmail.it>
2018-12-05 15:37:10 +0100

    fixed bugs

--- assignment3/assignment3_andrea/Computer_Exercise_1.m
+++ assignment3/assignment3_andrea/Computer_Exercise_1.m
@@ -19,10 +19,10 @@ if normalization
     N = [s(1),0,-s(1)*m_(1);0,s(2),-s(2)*m_(2);0,0,1];
         if i==1
             N1 = N;
-            x1_norm = N*x1;
+            x1_norm = N1*x1;
         else
             N2 = N;
-            x2_norm = N*x2;
+            x2_norm = N2*x2;
         end
     end
 else
@@ -46,17 +46,18 @@ fprintf("\nthe ||v|| is :%f",norm(sol));
 fprintf("\nthe eigenvalue is :%f",S(end,end));
 fprintf("\nthe ||Mv|| is :%f",norm(M*sol));
 
-F_old = reshape( sol ,[3 3])';
-[U ,S ,V] = svd (F_old)
-ss = diag(S);
-F_old = U*diag([ss(1:end-1);0])*V ;
+F_old = reshape( sol ,[3 3])
+[U ,S ,V] = svd (F_old);
+S(3,3) = 0;
+F_old = U*S*V' ;
 fprintf("determinant of F Normalized is %d\n",det(F_old)  )
 indice = 2;
 fprintf("Checking for the %d point the epipolar constrain we get: %d\n",...
         indice,x2_norm(1:3,indice)'*F_old*x1_norm(1:3,indice) )
 
 %Getting the original Matrix:    
-F = N2'*F_old*N1 ; 
+F = N2'*F_old*N1 ;
+F = F./F(3,3);
 
 l = F*x {1}; % Computes the epipolar lines
 l = l ./ sqrt ( repmat ( l (1 ,:).^2 + l (2 ,:).^2 ,[3 1]));
